57959aa28af15e410a2a4ef27ecbe4838f2b3e78
Itto Okumura <itto@ItMac.local>
2025-06-08 00:35:18 +0900

    add: lp/footer

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -4,6 +4,8 @@ import "../../app/globals.css";
 import React from "react";
 import AuthProvider from "@/components/auth/AuthProvider";
 import Link from "next/link";
+import { Footer } from "./Footer";
+import Header from "./Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,42 +29,7 @@ export interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
-      <header className="sticky top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-sm shadow">
-        <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <Link
-                href="/admin"
-                className="text-xl font-bold text-gray-900 hover:text-[#00a497] transition-colors"
-              >
-                It.dev
-              </Link>
-            </div>
-            <div>
-              <nav className="hidden md:flex space-x-4">
-                <Link
-                  href="#profile-title"
-                  className="text-gray-600 hover:text-[#00a497] transition-colors"
-                >
-                  Profile
-                </Link>
-                <Link
-                  href="#services-title"
-                  className="text-gray-600 hover:text-[#00a497] transition-colors"
-                >
-                  Services
-                </Link>
-                <Link
-                  href="#contact-title"
-                  className="text-gray-600 hover:text-[#00a497] transition-colors"
-                >
-                  Contact
-                </Link>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
       <html lang="ja">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -70,6 +37,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <AuthProvider>{children}</AuthProvider>
         </body>
       </html>
+      <Footer />
     </>
   );
 }
