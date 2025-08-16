2c4986ad08ddb9fbf14f8dcf9618d8fdeb83d989
Itto Okumura <itto.mura@gmail.com>
2025-08-16 23:51:28 +0900

    fix: firestore-rule

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
 				port: "",
 				pathname: "/v0/b/**",
 			},
+			{
+				protocol: "https",
+				hostname: "**.firebasestorage.app",
+				port: "",
+				pathname: "/o/**",
+			},
 		],
 	},
 };
