0866873fa773b81493e73c3462179e03da4bd311
Itto Okumura <itto.mura@gmail.com>
2025-08-17 17:27:31 +0900

    fix: conflict

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,9 @@ const nextConfig: NextConfig = {
 				pathname: "/**",
 			},
 		],
+		domains: ["firebasestorage.googleapis.com"],
+		dangerouslyAllowSVG: true,
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 	},
 };
 
